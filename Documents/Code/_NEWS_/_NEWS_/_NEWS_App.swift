2410a1cf63862defbd88c87e041085fade7af603
Esakkimuthu <esakimuthusivaraman@yahoo.com>
2023-05-22 12:19:07 +0530

    [Esakkimuthu] Push all files

---
+++ Documents/Code/_NEWS_/_NEWS_/_NEWS_App.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+@main
+struct _NEWS_App: App {
+    
+    @StateObject var articleBookmarkVM = ArticleBookmarkViewModel.shared
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(articleBookmarkVM)
+        }
+    }
+}
